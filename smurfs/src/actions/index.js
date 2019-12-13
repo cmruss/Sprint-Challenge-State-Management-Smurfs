b29d833cf4236d8c739425bd320f629d464ce18d
cmruss <cmrussell08@gmail.com>
2019-12-13 13:36:28 -0500

    MVP

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,8 @@
+export {
+    POST_SMURF,
+    FETCH_SMURF_START,
+    FETCH_SMURF_SUCCESS,
+    FETCH_SMURF_FAILURE,
+    getSmurfs,
+    giveSmurf
+} from './smurfAction';
---
+++ smurfs/src/actions/smurfAction.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+export const POST_SMURF = 'POST_SMURF';
+export const FETCH_SMURF_START = 'FETCH_SMURF_START';
+export const FETCH_SMURF_SUCCESS = 'FETCH_SMURF_SUCCESS';
+export const FETCH_SMURF_FAILURE = 'FETCH_SMURF_FAILURE';
+
+export const getSmurfs = () => dispatch => {
+
+    dispatch({ type: FETCH_SMURF_START });
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then(response => {
+            console.log(response.data)
+            dispatch({ type: FETCH_SMURF_SUCCESS, payload: response.data })
+        })
+        .catch(error => {
+            dispatch({ type: FETCH_SMURF_FAILURE, payload: error.response })
+        })
+}
+
+export const giveSmurf = (newSmurf) => dispatch => {
+    dispatch({ type: POST_SMURF, payload: newSmurf });
+    axios
+        .post('http://localhost:3333/smurfs', newSmurf)
+        .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    dispatch({ type: FETCH_SMURF_START });
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then(response => {
+            console.log(response.data)
+            dispatch({ type: FETCH_SMURF_SUCCESS, payload: response.data })
+        })
+        .catch(error => {
+            dispatch({ type: FETCH_SMURF_FAILURE, payload: error.response })
+        })
+}
